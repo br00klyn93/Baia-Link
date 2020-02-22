5a42100e3e697cada00153c41f20bbc09568026f
br00klyn93 <bmclaury93@gmail.com>
2020-02-22 13:51:51 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, redirect
+from flask import make_response, Response
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from pprint import pprint
+
+app = Flask(__name__)
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+playlists = sp.user_playlists('digitalageb')
+
+pl_id = 'spotify:playlist:02ffdMW3wCP6CWLXtKh7vb'
+offset = 0
+
+song_ids = []
+
+@app.route('/')
+def main():
+    get_spotify()
+    display()
+
+# On post request => get_spotify() then display()
+
+def get_spotify():
+    while True:
+        response = sp.playlist_tracks(pl_id,offset=offset,fields='items.track.name,total')
+        for i in response['items']:
+            ye = str(i)
+            ye = ye[23:-3]
+
+            if ye == '':
+                continue
+            else:
+                song_ids.append(ye)
+                print(ye)
+
+        offset = offset + len(response['items'])
+        print("offset: ", offset, "/", response['total'])
+
+        if len(response['items']) == 0:
+            break
+
+
+def display():
+    return("\n".join(song_ids))
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=5000)
