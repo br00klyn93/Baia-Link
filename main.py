214ee400d0218afb7ba77875088d42bdde92323e
br00klyn93 <bmclaury93@gmail.com>
2020-02-22 17:18:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def get_spotify():
         print("ye: ", ye.partition("type")[2].partition("name")[2])
         name = ye[10:].partition("type")[2]
         name = name.partition("name")[2]
-        if len(name) > 30:
+        if name.partition("name")[2] != '':
             name = name.partition("name")[2]
             name = name[4:-3]
         else:
